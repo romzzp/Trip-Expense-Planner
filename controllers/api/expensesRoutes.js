ac05481cad8a25b4938626637afccb3b5479703f
Altug Cakmakci <altugcakmakci@gmail.com>
2021-10-27 12:59:50 -0400

    api routes

---
+++ controllers/api/expensesRoutes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { Expenses } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newExpense = await Expenses.create({
+      trip_id: req.body.trip_id,
+      category: req.body.category,
+      budget: req.body.budget,
+      spent: req.body.spent,
+    });
+
+    res.status(200).json(newExpense);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id',  async (req, res) => {
+  // update a blogpost by its `id` value
+  try {
+    const tripData = await Expenses.update({
+      trip_id: req.body.trip_id,
+      category: req.body.category,
+      budget: req.body.budget,
+      spent: req.body.spent},
+      {where: {id : req.params.id}});
+    res.status(200).json(tripData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const tripData = await Expenses.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+
+    if (!tripData) {
+      res.status(404).json({ message: 'No expense found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tripData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
