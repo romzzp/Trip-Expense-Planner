fa9bd64d525677d71a1a11107a1cbea8173e92ee
Talha636 <85548877+Talha636@users.noreply.github.com>
2021-10-28 13:27:58 -0400

    trip routes handlebar

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,14 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const name = document.querySelector('#trip-name').value.trim();
+  const needed_budget = document.querySelector('#trip-budget').value.trim();
+  const description = document.querySelector('#trip-desc').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+  if (name && needed_budget && description) {
+    const response = await fetch(`/api/trips`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ name, needed_budget, description }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -17,7 +17,7 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create trip');
     }
   }
 };
@@ -26,22 +26,22 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/trips/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to delete project');
+      alert('Failed to delete trip');
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
+  .querySelector('.new-trip-form')
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('.trip-list')
   .addEventListener('click', delButtonHandler);
